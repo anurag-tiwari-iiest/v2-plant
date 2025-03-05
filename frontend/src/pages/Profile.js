bd47dfd02b84a5f17713b7f2542779c237b4c962
AnurAg Personal <2020eeb087.anurag@students.iiests.ac.in>
2025-03-05 12:59:51 +0530

    Fixed settings 4

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { API_URL } from "../config";
 
 const Profile = () => {
   const [plants, setPlants] = useState([]);
